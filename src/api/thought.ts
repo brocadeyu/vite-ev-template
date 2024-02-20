813ccea810a63b4923438d093da55e3e4bad64d9
brocadeyu <yjb1725594062@gmail.com>
2024-02-20 10:25:18 +0800

    feat: 模型资源页面

--- src/api/thought.ts
+++ src/api/thought.ts
@@ -1,31 +1,15 @@
 import http from '@/utils/http'
-// export const getThoughtList = () =>
-//   new Promise((resolve, reject) => {
-//     http({
-//       url: '/assumption/list',
-//       method: 'post',
-//       data: {
-//         pageInfo: {
-//           pageNum: 1,
-//           pageSize: 100
-//         }
-//       }
-//     })
-//       .then((res) => {
-//         if (res.code === 200) {
-//           resolve(res.result)
-//         }
-//       })
-//       .catch((e) => {
-//         reject(e)
-//       })
-//   })
-
 export const getThoughtList = () =>
   new Promise((resolve, reject) => {
     http({
-      url: '/test',
-      method: 'get'
+      url: '/assumption/list',
+      method: 'post',
+      data: {
+        pageInfo: {
+          pageNum: 1,
+          pageSize: 100
+        }
+      }
     })
       .then((res) => {
         if (res.code === 200) {
@@ -36,3 +20,19 @@ export const getThoughtList = () =>
         reject(e)
       })
   })
+
+// export const getThoughtList = () =>
+//   new Promise((resolve, reject) => {
+//     http({
+//       url: '/test',
+//       method: 'get'
+//     })
+//       .then((res) => {
+//         if (res.code === 200) {
+//           resolve(res.result)
+//         }
+//       })
+//       .catch((e) => {
+//         reject(e)
+//       })
+//   })
--- src/views/thought-edit/components/model-source.vue
+++ src/views/thought-edit/components/model-source.vue
@@ -1,15 +1,47 @@
 <template>
   <div class="ms-container">
-    <BaseDocker
-      :title="'模型资源'"
-      :height="'200px'"
-      :width="'200px'"
-    ></BaseDocker>
+    <BaseDocker :title="'模型资源'" :height="'200px'" :width="'200px'">
+      <template #content>
+        <div class="card-container">
+          <div
+            v-for="(item, index) in modelConfig"
+            :key="index"
+            class="card-item"
+          >
+            <img :src="item.img" width="60px" height="60px" />
+            {{ item.name }}
+          </div>
+        </div>
+      </template>
+    </BaseDocker>
   </div>
 </template>
 
 <script setup lang="ts">
 import BaseDocker from '@/components/BaseDocker.vue'
+import { ref } from 'vue'
+import WZImg from '@/assets/model/wzzsj.png'
+import ZCImg from '@/assets/model/zczsj.png'
+import SHIP052C from '@/assets/model/052c.png'
+import SHIP052D from '@/assets/model/052d.png'
+const modelConfig = ref([
+  {
+    name: '武装直升机',
+    img: WZImg
+  },
+  {
+    name: '侦察直升机',
+    img: ZCImg
+  },
+  {
+    name: '中国052C型驱逐舰',
+    img: SHIP052C
+  },
+  {
+    name: '中国052D型驱逐舰',
+    img: SHIP052D
+  }
+])
 </script>
 
 <style scoped>
@@ -17,5 +49,24 @@ import BaseDocker from '@/components/BaseDocker.vue'
   position: fixed;
   top: 11vh;
   left: 20px;
+}
+.card-container {
+  display: flex;
+  height: 100%;
+  width: 100%;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  align-items: center;
+  box-sizing: border-box;
+  padding: 10px 0;
+}
+.card-item {
+  width: 65px;
+  height: 65px;
+  font-size: 12px;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
 }
 </style>
