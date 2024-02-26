f46caf6fdea8e6a9a5d092f56c6a08da767b0c08
brocadeyu <yjb1725594062@gmail.com>
2024-02-26 09:33:03 +0800

    feat: 顶部通用按钮

--- src/views/thought-main.vue
+++ src/views/thought-main.vue
@@ -1,14 +1,45 @@
 <template>
-  <BaseHeader :title="computeTitle"></BaseHeader>
+  <BaseHeader :title="computeTitle">
+    <div class="header-btn">
+      <el-button
+        v-if="!['数据链模拟', '想定回放'].includes(computeTitle)"
+        type="primary"
+        size="small"
+        >保存</el-button
+      >
+      <el-button
+        v-if="computeTitle !== '数据链模拟'"
+        type="primary"
+        size="small"
+        @click="back"
+        >返回</el-button
+      >
+    </div>
+  </BaseHeader>
   <RouterView></RouterView>
 </template>
 
 <script setup lang="ts">
 import { computed } from 'vue'
-import { useRoute } from 'vue-router'
+import { useRoute, useRouter } from 'vue-router'
+const route = useRoute()
+const router = useRouter()
 const computeTitle = computed(() => {
-  return useRoute().name as string
+  return route.name as string
 })
+const back = () => {
+  router.back()
+}
 </script>
 
-<style scoped></style>
+<style scoped>
+.header-btn {
+  width: 100%;
+  /* background-color: red; */
+  display: flex;
+  justify-content: flex-end;
+  padding: 0 30px;
+  transform: translateY(-3px);
+  align-items: center;
+}
+</style>
