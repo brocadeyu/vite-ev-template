a144a2b39b42e86e06b77ef0c052e000faca73b5
brocadeyu <yjb1725594062@gmail.com>
2024-02-29 16:10:15 +0800

    feat: 图层控制接口调用

--- src/views/thought-edit/components/layer-controller.vue
+++ src/views/thought-edit/components/layer-controller.vue
@@ -6,11 +6,15 @@
           <el-tree
             :data="data"
             :props="defaultProps"
-            style="width: 100%; height: 100%"
+            style="width: 100%; height: 100%; user-select: none"
             accordion
             show-checkbox
+            :default-checked-keys="['模型', '轨迹', '数据链']"
+            node-key="label"
             default-expand-all
+            check-on-click-node
             @node-click="handleNodeClick"
+            @check-change="handleCurrentChange"
           />
         </div>
       </template>
@@ -20,12 +24,70 @@
 
 <script setup lang="ts">
 import BaseDocker from '@/components/BaseDocker.vue'
+import { useCesiumStore } from '@/stores/cesiumStore'
 interface Tree {
-  label: string
+  label: keyof typeof NodeLabel
   children?: Tree[]
 }
+enum NodeLabel {
+  '模型',
+  '轨迹',
+  '数据链',
+  '综合链',
+  '90X链',
+  'JIDS链',
+  'KU卫通'
+}
+const cesiumStore = useCesiumStore()
+const handleCurrentChange = (node: any, isChecked: boolean) => {
+  const nodeLabel = node.label
+  switch (nodeLabel) {
+    case '模型':
+      break
+    case '轨迹':
+      break
+    // case '数据链':
+    //   break
+    case '综合链':
+      cesiumStore.cesium.linkMap.setVisibleByType({
+        type: '综合链',
+        flag: isChecked
+      })
+      break
+    case '90X链':
+      cesiumStore.cesium.linkMap.setVisibleByType({
+        type: '90X链',
+        flag: isChecked
+      })
+      break
+    case 'JIDS链':
+      cesiumStore.cesium.linkMap.setVisibleByType({
+        type: 'JIDS链',
+        flag: isChecked
+      })
+      break
+    case 'KU卫通':
+      break
+  }
+}
 const handleNodeClick = (data: Tree) => {
-  console.log(data)
+  const nodeLabel = data.label
+  switch (nodeLabel) {
+    case '模型':
+      break
+    case '轨迹':
+      break
+    case '数据链':
+      break
+    case '综合链':
+      break
+    case '90X链':
+      break
+    case 'JIDS链':
+      break
+    case 'KU卫通':
+      break
+  }
 }
 
 const data: Tree[] = [
