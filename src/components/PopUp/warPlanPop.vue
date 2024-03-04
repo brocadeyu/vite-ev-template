36840486797794e5805b3ffda0df2bdce6bdef24
brocadeyu <yjb1725594062@gmail.com>
2024-03-04 17:02:19 +0800

    feat: 作战计划ui

--- src/components/PopUp/warPlanPop.vue
+++ src/components/PopUp/warPlanPop.vue
@@ -2,16 +2,119 @@
   <BaseDocker
     :title="title"
     :show-footer="true"
-    :height="'400px'"
-    :width="'450px'"
+    :height="'410px'"
+    :width="'600px'"
     :is-draggable="true"
   >
+    <template #content>
+      <div class="mission-content">
+        <el-tabs v-model="activeTab" class="demo-tabs" @tab-click="handleClick">
+          <el-tab-pane label="静态" name="static">
+            <el-scrollbar height="278px">
+              <draggable
+                :list="staticList"
+                :animation="150"
+                item-key="order"
+                class="list-group"
+                ghost-class="ghost"
+              >
+                <template #item="{ element }">
+                  <div class="list-group-item">
+                    <div class="item-left">T0+:</div>
+                    <div class="item-center">
+                      <el-input-number
+                        v-model="element.time"
+                        :min="1"
+                        :max="9999"
+                        size="small"
+                        style="
+                          --el-fill-color-blank: #0c1a39;
+                          --el-border-color: #0e9aa0;
+                          --el-border-color-hover: #16b0b8;
+                          --el-fill-color-light: #119aa0;
+                          --el-border-color: #0e9aa0;
+                          --el-text-color-regular: #b9b5b5;
+                        "
+                        controls-position="right"
+                      />
+                    </div>
+                    <div class="item-right">
+                      <el-input
+                        v-model="element.message"
+                        size="small"
+                        style="
+                          --el-input-bg-color: #0c1a39;
+                          --el-input-border-color: #0e9aa0;
+                          --el-input-hover-border-color: #16b0b8;
+                          --el-text-color-regular: #b9b5b5;
+                        "
+                        placeholder="Please input"
+                      />
+                    </div>
+                  </div>
+                </template>
+              </draggable>
+            </el-scrollbar>
+          </el-tab-pane>
+          <el-tab-pane label="动态" name="dynamic">
+            <el-scrollbar height="278px">
+              <draggable
+                :list="dynamicList"
+                :animation="150"
+                item-key="order"
+                class="list-group"
+                ghost-class="ghost"
+              >
+                <template #item="{ element }">
+                  <div class="list-group-item">
+                    <div class="item-left">T0+:</div>
+                    <div class="item-center">
+                      <el-input-number
+                        v-model="element.time"
+                        :min="1"
+                        :max="9999"
+                        size="small"
+                        controls-position="right"
+                        style="
+                          --el-fill-color-blank: #0c1a39;
+                          --el-border-color: #0e9aa0;
+                          --el-border-color-hover: #16b0b8;
+                          --el-fill-color-light: #119aa0;
+                          --el-border-color: #0e9aa0;
+                          --el-text-color-regular: #b9b5b5;
+                        "
+                      />
+                    </div>
+                    <div class="item-right">
+                      <el-input
+                        v-model="element.message"
+                        size="small"
+                        style="
+                          --el-input-bg-color: #0c1a39;
+                          --el-input-border-color: #0e9aa0;
+                          --el-input-hover-border-color: #16b0b8;
+                          --el-text-color-regular: #b9b5b5;
+                        "
+                        placeholder="Please input"
+                      />
+                    </div>
+                  </div>
+                </template>
+              </draggable>
+            </el-scrollbar>
+          </el-tab-pane>
+        </el-tabs>
+      </div>
+    </template>
     <template #footer>
       <div class="foot-btns">
         <el-button type="primary" size="small" @click="closePopup"
-          >取消</el-button
-        >
+          >取消
+        </el-button>
+        <el-button type="primary" size="small">生成</el-button>
+        <el-button type="primary" size="small">导入</el-button>
         <el-button type="primary" size="small">保存</el-button>
+        <el-button type="primary" size="small">下发</el-button>
       </div>
     </template>
   </BaseDocker>
@@ -19,8 +122,20 @@
 
 <script setup lang="ts">
 import BaseDocker from '@/components/BaseDocker.vue'
+import draggable from 'vuedraggable'
 import { usePopupStore } from '@/stores/popupStore'
+import { useMissionStore } from '@/stores/missionStore'
+import type { TabsPaneContext } from 'element-plus'
 const popupStore = usePopupStore()
+const missionStore = useMissionStore()
+const activeTab = ref('static')
+const staticList = ref([])
+const dynamicList = ref([])
+
+const drag = ref(false)
+const handleClick = (tab: TabsPaneContext, event: Event) => {
+  // console.log(tab, event)
+}
 const props = withDefaults(
   defineProps<{
     title?: string
@@ -31,11 +146,97 @@ const props = withDefaults(
 const closePopup = () => {
   popupStore.closePop()
 }
+onMounted(() => {
+  staticList.value = missionStore.staticMission
+  dynamicList.value = missionStore.dynamicMission
+  console.log('静态', staticList.value)
+  console.log('动态', dynamicList.value)
+})
 </script>
 
 <style scoped>
 .foot-btns {
   display: flex;
   justify-content: flex-end;
+  margin-right: 2px;
+}
+.mission-content {
+  height: 100%;
+  padding: 5px;
+  box-sizing: border-box;
+}
+::v-deep(.el-tabs__item) {
+  --el-text-color-primary: #245b94;
+}
+::v-deep(.el-tabs__nav-wrap::after) {
+  --el-border-color-light: #133757;
+}
+::v-deep(.el-input-number.is-controls-right .el-input-number__increase) {
+  border-color: #64a7b9;
+  bottom: 0px;
+}
+::v-deep(.el-input-number.is-controls-right .el-input-number__decrease) {
+  border-color: #64a7b9;
+  bottom: 0px;
+}
+::v-deep(.el-tabs .el-tabs__content .el-tab-pane) {
+  height: 100%;
+}
+::v-deep(.el-input-number__decrease) {
+  color: white;
+}
+::v-deep(.el-input-number__increase) {
+  color: white;
+}
+::v-deep(.el-input-number__decrease.is-disabled) {
+  color: #3d4b6c;
+}
+
+.ghost {
+  opacity: 0.5;
+  background: #c8ebfb;
+}
+
+.list-group {
+  /* height: 100%; */
+  /* background-color: #215570; */
+  display: flex;
+  flex-direction: column;
+}
+
+.list-group-item {
+  cursor: move;
+  height: 40px;
+  display: flex;
+}
+.item-left {
+  flex: 1;
+  color: #b9b5b5;
+  margin-left: 5px;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+}
+.item-center {
+  flex: 3;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.item-right {
+  flex: 7;
+  color: white;
+  /* border: 2px solid beige; */
+  /* border-radius: 4px; */
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  font-size: 14px;
+  margin: 5px 12px;
+  padding: 10px;
+}
+
+.list-group-item i {
+  cursor: pointer;
 }
 </style>
