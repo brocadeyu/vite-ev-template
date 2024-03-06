2e426df98f5181503c83e57825cdccd52e0f4f40
brocadeyu <yjb1725594062@gmail.com>
2024-03-06 16:07:05 +0800

    feat: 回放初始化连接ws

--- src/views/thought-replay/thought-replay.vue
+++ src/views/thought-replay/thought-replay.vue
@@ -4,5 +4,27 @@
 </template>
 <script setup lang="ts">
 import controlBar from './components/control-bar.vue'
+import { useWebSocketStore } from '@/stores/webSocketStore'
+import { WS_EVENT } from '@/common/enum'
+import { ElMessage } from 'element-plus'
+const websocketStore = useWebSocketStore()
+onMounted(() => {
+  websocketStore.connect('ws://localhost:12000/hsdb/101')
+  websocketStore.addEventListener(WS_EVENT.onopen, () => {
+    // eslint-disable-next-line no-console
+    console.log('连接websocket')
+  })
+  websocketStore.addEventListener(WS_EVENT.onclose, () => {
+    // eslint-disable-next-line no-console
+    console.log('断开websocket')
+  })
+  websocketStore.addEventListener(WS_EVENT.onerror, () => {
+    ElMessage.error(`websocket出错!`)
+  })
+})
+
+onBeforeUnmount(() => {
+  websocketStore.disconnect()
+})
 </script>
 <style scoped></style>
