f8708c463d7d0965c7112b92143d19ad8cecfee2
brocadeyu <yjb1725594062@gmail.com>
2024-03-07 09:39:09 +0800

    feat: 回放初始化加载模型

--- src/views/thought-replay/thought-replay.vue
+++ src/views/thought-replay/thought-replay.vue
@@ -8,12 +8,15 @@ import controlBar from './components/control-bar.vue'
 import loadingMask from './components/loading-mask.vue'
 import { useWebSocketStore } from '@/stores/webSocketStore'
 import { useThoughtStore } from '@/stores/thougthStore'
+import { useEntityStore } from '@/stores/entityStore'
+import { useCesiumStore } from '@/stores/cesiumStore'
 import { WS_EVENT } from '@/common/enum'
 import { ElMessage } from 'element-plus'
 const loadingMaskRef = ref(null)
 const websocketStore = useWebSocketStore()
 const thoughtStore = useThoughtStore()
-
+const entityStore = useEntityStore()
+const cesiumStore = useCesiumStore()
 const initSendMessage = () => {
   const thought = thoughtStore.thought
   let data = {
@@ -40,6 +43,11 @@ onMounted(() => {
   websocketStore.addEventListener(WS_EVENT.initialSuccess, () => {
     console.log('初始化加载想定成功')
     loadingMaskRef.value.endProgress()
+  })
+  entityStore.initEntityStore(thoughtStore.thought.entities) //根据thought初始化entityStore
+  entityStore.entitiesArr.forEach((_) => {
+    cesiumStore.cesium.modelMap.addModel(_ as any)
+    // cesiumStore.cesium.trackMap.addTrack({ id: _.id, positionArr: _.path })
   })
 })
 
