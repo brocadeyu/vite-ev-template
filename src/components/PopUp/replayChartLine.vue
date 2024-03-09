b5b851b5f866ffee8d3901ee70491756623a91cd
brocadeyu <yjb1725594062@gmail.com>
2024-03-10 02:11:55 +0800

    feat: 动态折线图

--- src/components/PopUp/replayChartLine.vue
+++ src/components/PopUp/replayChartLine.vue
@@ -7,7 +7,7 @@
     :is-draggable="true"
   >
     <template #content>
-      <div class="mission-content"></div>
+      <div ref="lineChartRef" class="mission-content"></div>
     </template>
     <template #footer>
       <div class="foot-btns">
@@ -25,10 +25,16 @@
 
 <script setup lang="ts">
 import BaseDocker from '@/components/BaseDocker.vue'
+import * as echarts from 'echarts'
 import { usePopupStore } from '@/stores/popupStore'
+import { useLogStore } from '@/stores/logStore'
 import { storeToRefs } from 'pinia'
 const popupStore = usePopupStore()
-const props = withDefaults(
+const logStore = useLogStore()
+const { dataLog } = storeToRefs(logStore)
+const lineChartRef = ref<HTMLElement>()
+let echartsInstance = null
+withDefaults(
   defineProps<{
     title?: string
   }>(),
@@ -38,9 +44,216 @@ const props = withDefaults(
 const closePopup = () => {
   popupStore._showLine = false
 }
+const initEcharts = () => {
+  echartsInstance = echarts.init(lineChartRef.value)
+}
+const colors = ['#5470C6', '#91CC75', '#EE6666', '#1E7666']
+let data = []
+// let now = new Date(1997, 9, 3)
+// let oneDay = 24 * 3600 * 1000
+// let value = Math.random() * 1000
+// const randomData = () => {
+//   now = new Date(+now + oneDay)
+//   value = value + Math.random() * 21 - 10
+//   return {
+//     name: now.toString(),
+//     value: [
+//       [now.getFullYear(), now.getMonth() + 1, now.getDate()].join('/'),
+//       Math.round(value)
+//     ]
+//   }
+// }
+// for (var i = 0; i < 1000; i++) {
+//   data.push(randomData())
+// }
+const setEchartsData = () => {
+  const option = {
+    color: colors,
+    tooltip: {
+      trigger: 'axis',
+      axisPointer: {
+        type: 'cross'
+      }
+    },
+    grid: {
+      right: '20%'
+    },
+    legend: {
+      data: ['时延', '带宽占用率', '丢包率', '速率']
+    },
+    xAxis: [
+      {
+        type: 'time',
+        splitLine: {
+          show: false
+        },
+        axisLabel: {
+          color: '#7ec7ff',
+          interval: 0,
+          formatter: function (params) {
+            // return params;
+            // console.log(params)
+            return `T0+${String(params)}s`
+          }
+        }
+      }
+    ],
+    yAxis: [
+      {
+        type: 'value',
+        name: '时延',
+        position: 'right',
+        alignTicks: true,
+        axisLine: {
+          show: true,
+          lineStyle: {
+            color: colors[0]
+          }
+        },
+        axisLabel: {
+          formatter: '{value} ms'
+        }
+      },
+      {
+        type: 'value',
+        name: '带宽占用率',
+        position: 'right',
+        alignTicks: true,
+        offset: 80,
+        axisLine: {
+          show: true,
+          lineStyle: {
+            color: colors[1]
+          }
+        },
+        axisLabel: {
+          formatter: '{value}%'
+        }
+      },
+      {
+        type: 'value',
+        name: '丢包率',
+        position: 'left',
+        alignTicks: true,
+        axisLine: {
+          show: true,
+          lineStyle: {
+            color: colors[2]
+          }
+        },
+        axisLabel: {
+          formatter: '{value}%'
+        }
+      },
+      {
+        type: 'value',
+        name: '速率',
+        offset: 37,
+        position: 'left',
+        alignTicks: true,
+        axisLine: {
+          show: true,
+          lineStyle: {
+            color: colors[3]
+          }
+        },
+        axisLabel: {
+          formatter: '{value}kb/s'
+        }
+      }
+    ],
+    series: [
+      {
+        name: '时延',
+        type: 'line',
+        yAxisIndex: 0,
+        showSymbol: false,
+        // smooth: true,
+        data: []
+      },
+      {
+        name: '带宽占用率',
+        type: 'line',
+        yAxisIndex: 1,
+        showSymbol: false,
+        // smooth: true,
+        data: []
+      },
+      {
+        name: '丢包率',
+        type: 'line',
+        yAxisIndex: 2,
+        showSymbol: false,
+        // smooth: true,
+        data: []
+      },
+      {
+        name: '速率',
+        type: 'line',
+        yAxisIndex: 3,
+        showSymbol: false,
+        // smooth: true,
+        data: []
+      }
+    ]
+  }
+  echartsInstance.setOption(option)
+}
+let timer
 onMounted(() => {
-  // console.log('静态', staticList.value)
-  // console.log('动态', dynamicList.value)
+  initEcharts()
+  setEchartsData()
+  timer = setInterval(() => {
+    data.push({
+      delay: 1 + 1 * Math.random(),
+      missPercent: Math.random(),
+      speed: 1 * Math.random(),
+      speedPercent: Math.random(),
+      time: Date.now()
+    })
+    if (data.length > 20) {
+      data.shift()
+    }
+    echartsInstance.setOption({
+      series: [
+        {
+          name: '时延',
+          type: 'line',
+          yAxisIndex: 0,
+          data: data.map((_) => {
+            return { name: _.time, value: [_.time, _.delay] }
+          })
+        },
+        {
+          name: '带宽占用率',
+          type: 'line',
+          yAxisIndex: 1,
+          data: data.map((_) => {
+            return { name: _.time, value: [_.time, _.missPercent] }
+          })
+        },
+        {
+          name: '丢包率',
+          type: 'line',
+          yAxisIndex: 2,
+          data: data.map((_) => {
+            return { name: _.time, value: [_.time, _.speed] }
+          })
+        },
+        {
+          name: '速率',
+          type: 'line',
+          yAxisIndex: 3,
+          data: data.map((_) => {
+            return { name: _.time, value: [_.time, _.speedPercent] }
+          })
+        }
+      ]
+    })
+  }, 1000)
+})
+onBeforeUnmount(() => {
+  clearInterval(timer)
 })
 </script>
 
