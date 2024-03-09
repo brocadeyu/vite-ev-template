e560af0936f36366574189473bb38b55c06c674e
brocadeyu <yjb1725594062@gmail.com>
2024-03-09 10:06:51 +0800

    chore: 删除无用代码

--- src/map/globeRotate.ts
+++ src/map/globeRotate.ts
@@ -1,17 +1,12 @@
 import type { Viewer } from 'cesium'
-import { Cartesian3 } from 'cesium'
-import { JulianDate } from 'cesium'
 import * as Cesium from 'cesium'
 export default class GlobalRotate {
   private viewer: Viewer = null
   private callback: () => void
-  previousTime: any
-  spinRate: 0.001
   constructor(_viewer: Viewer) {
     this.viewer = _viewer
-    this.previousTime = Date.now()
   }
-  public start(rate: number = 1) {
+  public start(rate: number = 0.01) {
     this.callback = () => {
       this.viewer.scene.camera.rotate(Cesium.Cartesian3.UNIT_Z, rate * 0.00007)
     }
@@ -21,7 +16,6 @@ export default class GlobalRotate {
     this.stop()
     this.start(rate)
   }
-
   public stop() {
     this.viewer.clock.onTick.removeEventListener(this.callback)
   }
