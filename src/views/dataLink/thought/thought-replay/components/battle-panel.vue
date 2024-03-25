203f7f8b4f41c62e86ee65cc9dd7133303e1d4e9
brocadeyu <yjb1725594062@gmail.com>
2024-03-25 11:00:52 +0800

    feat: 日志组件全屏

--- src/views/dataLink/thought/thought-replay/components/battle-panel.vue
+++ src/views/dataLink/thought/thought-replay/components/battle-panel.vue
@@ -7,64 +7,76 @@
       icon="log"
     >
       <template #header>
-        <el-button
-          type="primary"
-          color="#119aa0"
-          size="small"
-          @click="openGantChart"
-        >
-          <el-icon :size="16"><i-ep-PieChart /></el-icon>
-        </el-button>
+        <div>
+          <el-button
+            type="primary"
+            color="#119aa0"
+            size="small"
+            @click="goFullScreen"
+          >
+            <el-icon :size="16"><i-epFullScreen /></el-icon>
+          </el-button>
+          <el-button
+            type="primary"
+            color="#119aa0"
+            size="small"
+            @click="openGantChart"
+          >
+            <el-icon :size="16"><i-ep-PieChart /></el-icon>
+          </el-button>
+        </div>
       </template>
       <template #content>
-        <!-- <div class="bp-content"> -->
-        <div class="content-tree">
-          <el-input
-            v-model="queryStr"
-            placeholder="输入关键字搜索"
-            size="small"
-            style="
-              --el-input-bg-color: #0c1a39;
-              --el-input-border-color: #0e9aa0;
-              --el-input-hover-border-color: #16b0b8;
-            "
-            @input="onQueryChanged"
-          />
-          <el-tree-v2
-            ref="treeRef"
-            :data="data"
-            :props="props"
-            style="
-              /* background-color: #113751; */
-              background-color: black;
-              /* --el-tree-node-hover-bg-color: #0e9aa0; */
-              --el-tree-node-hover-bg-color: #393939;
-              color: white;
-              margin-top: 5px;
-              outline: 1px solid #0e9aa0;
-              border-radius: 4px;
-              height: 100%;
-              overflow: hidden;
-              --el-font-size-base: 12px;
-            "
-            :filter-method="filterMethod"
-            :height="150"
-            empty-text="暂无数据"
-          >
-            <template #default="{ node }">
-              <span style="transform: translateX(-18px); color: #15b1b7"
-                >>{{ node.data.timeStr }}</span
-              >
-              <span style="color: #dbdb32">{{
-                `T0+${node.data.timeT}ms:`
-              }}</span>
-              <span style="margin-left: 5px; color: #13d713">{{
-                node.data.label
-              }}</span>
-            </template>
-          </el-tree-v2>
+        <div ref="bpRef" class="bp-content">
+          <div class="content-tree">
+            <el-input
+              v-model="queryStr"
+              placeholder="输入关键字搜索"
+              size="small"
+              style="
+                --el-input-bg-color: #0c1a39;
+                --el-input-border-color: #0e9aa0;
+                --el-input-hover-border-color: #16b0b8;
+              "
+              @input="onQueryChanged"
+            />
+            <el-tree-v2
+              ref="treeRef"
+              :data="data"
+              :props="props"
+              style="
+                /* background-color: #113751; */
+                background-color: black;
+                /* --el-tree-node-hover-bg-color: #0e9aa0; */
+                --el-tree-node-hover-bg-color: #393939;
+                color: white;
+                margin-top: 5px;
+                outline: 1px solid #0e9aa0;
+                border-radius: 4px;
+                height: 100%;
+                overflow: hidden;
+                --el-font-size-base: 12px;
+                --el-text-color-secondary: #119aa0;
+                --el-scrollbar-opacity: 0.8;
+              "
+              :filter-method="filterMethod"
+              :height="isFullscreen ? 1000 : 150"
+              empty-text="暂无数据"
+            >
+              <template #default="{ node }">
+                <span style="transform: translateX(-18px); color: #15b1b7"
+                  >>{{ node.data.timeStr }}</span
+                >
+                <span style="color: #dbdb32">{{
+                  `T0+${node.data.timeT}ms:`
+                }}</span>
+                <span style="margin-left: 5px; color: #13d713">{{
+                  node.data.label
+                }}</span>
+              </template>
+            </el-tree-v2>
+          </div>
         </div>
-        <!-- </div> -->
       </template>
     </BaseDocker>
   </div>
@@ -77,6 +89,10 @@ import { ElTreeV2 } from 'element-plus'
 import type { TreeNode } from 'element-plus/es/components/tree-v2/src/types'
 import { useLogStore } from '@/stores/logStore'
 import { storeToRefs } from 'pinia'
+import { useFullscreen } from '@vueuse/core'
+const bpRef = ref<HTMLElement | null>(null)
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const { isFullscreen, enter, exit, toggle } = useFullscreen(bpRef)
 const popupStore = usePopupStore()
 const logStore = useLogStore()
 const { warMissionLog } = storeToRefs(logStore)
@@ -87,6 +103,9 @@ const onQueryChanged = (query: string) => {
 }
 const setTreeData = (data: any) => {
   treeRef.value?.setData(data)
+}
+const goFullScreen = () => {
+  toggle()
 }
 const data = []
 const props = {
@@ -146,5 +165,11 @@ watch(
   padding-left: 2px;
   box-sizing: border-box;
   height: 100%;
+}
+.bp-content {
+  height: 100%;
+}
+:deep(.el-virtual-scrollbar) {
+  opacity: 1;
 }
 </style>
--- src/views/dataLink/thought/thought-replay/components/data-panel.vue
+++ src/views/dataLink/thought/thought-replay/components/data-panel.vue
@@ -7,61 +7,75 @@
       icon="log"
     >
       <template #header>
-        <el-button
-          type="primary"
-          color="#119aa0"
-          size="small"
-          @click="openDataChart"
-        >
-          <el-icon :size="16"><i-ep-DataAnalysis /></el-icon>
-        </el-button>
+        <div>
+          <el-button
+            type="primary"
+            color="#119aa0"
+            size="small"
+            @click="goFullScreen"
+          >
+            <el-icon :size="16"><i-epFullScreen /></el-icon>
+          </el-button>
+          <el-button
+            type="primary"
+            color="#119aa0"
+            size="small"
+            @click="openDataChart"
+          >
+            <el-icon :size="16"><i-ep-DataAnalysis /></el-icon>
+          </el-button>
+        </div>
       </template>
       <template #content>
-        <div class="content-tree">
-          <el-input
-            v-model="queryStr"
-            placeholder="输入关键字搜索"
-            size="small"
-            style="
-              --el-input-bg-color: #0c1a39;
-              --el-input-border-color: #0e9aa0;
-              --el-input-hover-border-color: #16b0b8;
-            "
-            @input="onQueryChanged"
-          />
-          <el-tree-v2
-            ref="treeRef"
-            :data="data"
-            :props="props"
-            style="
-              /* background-color: #113751; */
-              background-color: black;
-              /* --el-tree-node-hover-bg-color: #0e9aa0; */
-              --el-tree-node-hover-bg-color: #393939;
-              color: white;
-              margin-top: 5px;
-              outline: 1px solid #0e9aa0;
-              border-radius: 4px;
-              height: 100%;
-              overflow: hidden;
-              --el-font-size-base: 12px;
-            "
-            :filter-method="filterMethod"
-            :height="150"
-            empty-text="暂无数据"
-          >
-            <template #default="{ node }">
-              <span style="transform: translateX(-18px); color: #15b1b7"
-                >>{{ node.data.timeStr }}</span
-              >
-              <span style="color: #dbdb32"
-                >{{ node.data.label.split(':')[0] }}:</span
-              >
-              <span style="margin-left: 5px; color: #13d713">{{
-                node.data.label.split(':')[1]
-              }}</span>
-            </template>
-          </el-tree-v2>
+        <div ref="dpRef" class="dp-content">
+          <div class="content-tree">
+            <el-input
+              v-model="queryStr"
+              placeholder="输入关键字搜索"
+              size="small"
+              style="
+                --el-input-bg-color: #0c1a39;
+                --el-input-border-color: #0e9aa0;
+                --el-input-hover-border-color: #16b0b8;
+              "
+              @input="onQueryChanged"
+            />
+            <el-tree-v2
+              ref="treeRef"
+              :data="data"
+              :props="props"
+              style="
+                /* background-color: #113751; */
+                background-color: black;
+                /* --el-tree-node-hover-bg-color: #0e9aa0; */
+                --el-tree-node-hover-bg-color: #393939;
+                color: white;
+                margin-top: 5px;
+                outline: 1px solid #0e9aa0;
+                border-radius: 4px;
+                height: 100%;
+                overflow: hidden;
+                --el-font-size-base: 12px;
+                --el-text-color-secondary: #119aa0;
+                --el-scrollbar-opacity: 0.8;
+              "
+              :filter-method="filterMethod"
+              :height="isFullscreen ? 1000 : 150"
+              empty-text="暂无数据"
+            >
+              <template #default="{ node }">
+                <span style="transform: translateX(-18px); color: #15b1b7"
+                  >>{{ node.data.timeStr }}</span
+                >
+                <span style="color: #dbdb32"
+                  >{{ node.data.label.split(':')[0] }}:</span
+                >
+                <span style="margin-left: 5px; color: #13d713">{{
+                  node.data.label.split(':')[1]
+                }}</span>
+              </template>
+            </el-tree-v2>
+          </div>
         </div>
       </template>
     </BaseDocker>
@@ -75,6 +89,10 @@ import type { TreeNode } from 'element-plus/es/components/tree-v2/src/types'
 import { useLogStore } from '@/stores/logStore'
 import { usePopupStore } from '@/stores/popupStore'
 import { storeToRefs } from 'pinia'
+import { useFullscreen } from '@vueuse/core'
+const dpRef = ref<HTMLElement | null>(null)
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const { isFullscreen, enter, exit, toggle } = useFullscreen(dpRef)
 const queryStr = ref('')
 const logStore = useLogStore()
 const popupStore = usePopupStore()
@@ -82,6 +100,9 @@ const { dataLog } = storeToRefs(logStore)
 const treeRef = ref<InstanceType<typeof ElTreeV2>>()
 const onQueryChanged = (query: string) => {
   treeRef.value!.filter(query)
+}
+const goFullScreen = () => {
+  toggle()
 }
 const data = []
 const props = {
@@ -133,6 +154,9 @@ watch(
 :deep(.el-checkbox__input.is-checked .el-checkbox__inner) {
   background-color: rgb(47, 99, 244) !important;
 }
+:deep(.el-virtual-scrollbar) {
+  opacity: 1;
+}
 .content-tree {
   display: flex;
   flex-direction: column;
@@ -141,6 +165,9 @@ watch(
   padding-right: 2px;
   padding-left: 2px;
   box-sizing: border-box;
+  height: 100%;
+}
+.dp-content {
   height: 100%;
 }
 </style>
