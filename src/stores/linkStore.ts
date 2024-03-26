2941ecc239267ec733a3fa71fcc3daf3d5ea0fed
brocadeyu <yjb1725594062@gmail.com>
2024-03-26 14:11:19 +0800

    fix: 数据链初始化值

--- src/stores/linkStore.ts
+++ src/stores/linkStore.ts
@@ -16,7 +16,32 @@ interface LinkInfo {
 //   info: LinkInfo
 // }
 export const useLinkStore = defineStore('link', () => {
-  const linkConnectInfo = ref<Record<LinkType, LinkInfo>>({})
+  const linkConnectInfo = ref<Record<LinkType, LinkInfo>>({
+    综合链: {
+      linkTo: [],
+      mainDevice: [],
+      selection: [],
+      targetDevices: []
+    },
+    '90X链': {
+      linkTo: [],
+      mainDevice: [],
+      selection: [],
+      targetDevices: []
+    },
+    JIDS链: {
+      linkTo: [],
+      mainDevice: [],
+      selection: [],
+      targetDevices: []
+    },
+    KU卫通: {
+      linkTo: [],
+      mainDevice: [],
+      selection: [],
+      targetDevices: []
+    }
+  })
   const linklink = ref([])
   const setLinkConnectInfo = (type: LinkType, arg: LinkInfo) => {
     // const { type, info } = arg
@@ -26,7 +51,32 @@ export const useLinkStore = defineStore('link', () => {
     linklink.value = arg
   }
   const resetLink = () => {
-    linkConnectInfo.value = {}
+    linkConnectInfo.value = {
+      综合链: {
+        linkTo: [],
+        mainDevice: [],
+        selection: [],
+        targetDevices: []
+      },
+      '90X链': {
+        linkTo: [],
+        mainDevice: [],
+        selection: [],
+        targetDevices: []
+      },
+      JIDS链: {
+        linkTo: [],
+        mainDevice: [],
+        selection: [],
+        targetDevices: []
+      },
+      KU卫通: {
+        linkTo: [],
+        mainDevice: [],
+        selection: [],
+        targetDevices: []
+      }
+    }
     linklink.value = []
   }
   return {
